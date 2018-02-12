a0b042755fc564097b15a51e67e082fedc15b94c
Hector Sanjuan <code@hector.link>
2018-02-12 11:31:15 +0100

    Extract from go-ipfs: readme, travis, golint, makefile, tests...
    This commit was moved from ipfs/go-ipfs-ds-help@1c74d493369298b09646db74be972505b137d27e

--- datastore/dshelp/key.go
+++ datastore/dshelp/key.go
@@ -1,29 +1,33 @@
+// Package dshelp provides utilities for parsing and creating
+// datastore keys used by go-ipfs
 package dshelp
 
 import (
-	ds "gx/ipfs/QmPpegoMqhAEqjncrzArm7KVWAkCm78rqL2DPuNjhPrshg/go-datastore"
-	cid "gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
-	base32 "gx/ipfs/QmfVj3x4D6Jkq9SEoi5n2NmoUomLwoeiwnYz2KQa15wRw6/base32"
+	cid "github.com/ipfs/go-cid"
+	"github.com/ipfs/go-datastore"
+	"github.com/whyrusleeping/base32"
 )
 
-// TODO: put this code into the go-datastore itself
-
-func NewKeyFromBinary(rawKey []byte) ds.Key {
+// NewKeyFromBinary creates a new key from a byte slice.
+func NewKeyFromBinary(rawKey []byte) datastore.Key {
 	buf := make([]byte, 1+base32.RawStdEncoding.EncodedLen(len(rawKey)))
 	buf[0] = '/'
 	base32.RawStdEncoding.Encode(buf[1:], rawKey)
-	return ds.RawKey(string(buf))
+	return datastore.RawKey(string(buf))
 }
 
-func BinaryFromDsKey(k ds.Key) ([]byte, error) {
+// BinaryFromDsKey returns the byte slice corresponding to the given Key.
+func BinaryFromDsKey(k datastore.Key) ([]byte, error) {
 	return base32.RawStdEncoding.DecodeString(k.String()[1:])
 }
 
-func CidToDsKey(k *cid.Cid) ds.Key {
+// CidToDsKey creates a Key from the given Cid.
+func CidToDsKey(k *cid.Cid) datastore.Key {
 	return NewKeyFromBinary(k.Bytes())
 }
 
-func DsKeyToCid(dsKey ds.Key) (*cid.Cid, error) {
+// DsKeyToCid converts the given Key to its corresponding Cid.
+func DsKeyToCid(dsKey datastore.Key) (*cid.Cid, error) {
 	kb, err := BinaryFromDsKey(dsKey)
 	if err != nil {
 		return nil, err
