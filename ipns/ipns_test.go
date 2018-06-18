f706aa07d6f6b70b75a74c1a2f3005f979946a0a
Rob Brackett <rob@robbrackett.com>
2018-06-18 14:11:11 -0700

    Add `Create` example to godoc
    License: MIT
    Signed-off-by: Rob Brackett <rob@robbrackett.com>
    
    
    This commit was moved from ipfs/go-ipns@fa15ed2ecc1a7b2d198f160fcc6d5e94ad87e1c7

--- ipns/ipns_test.go
+++ ipns/ipns_test.go
@@ -41,3 +41,17 @@ func TestEmbedPublicKey(t *testing.T) {
 		t.Fatalf("pid mismatch: %s != %s", pid, embeddedPid)
 	}
 }
+
+func ExampleCreate() {
+	// Generate a private key to sign the IPNS record with. Most of the time,
+	// however, you'll want to retrieve an already-existing key from IPFS using
+	// go-ipfs/core/coreapi CoreAPI.KeyAPI() interface.
+	privateKey, publicKey, err := ic.GenerateKeyPair(ic.RSA, 2048)
+
+	// Create an IPNS record that expires in one hour and points to the IPFS address
+	// /ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5
+	ipnsRecord, err := Create(privateKey, []byte("/ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5"), 0, time.Now().Add(1*time.Hour))
+	if err != nil {
+		panic(err)
+	}
+}
