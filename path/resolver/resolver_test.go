c0127306b8eff01fc2073e1dfd144773777e6f07
Jeromy <jeromyj@gmail.com>
2018-07-28 23:37:36 -0700

    extractify
    This commit was moved from ipfs/go-path@a1a3981476082cc67ebce25af853ee767f3dfc04

--- path/resolver/resolver_test.go
+++ path/resolver/resolver_test.go
@@ -3,21 +3,23 @@ package resolver_test
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	path "github.com/ipfs/go-ipfs/path"
 	"github.com/ipfs/go-ipfs/path/resolver"
 	merkledag "gx/ipfs/QmRy4Qk9hbgFX9NGJRm8rBThrA8PZhNCitMgeRYyZ67s59/go-merkledag"
 	dagmock "gx/ipfs/QmRy4Qk9hbgFX9NGJRm8rBThrA8PZhNCitMgeRYyZ67s59/go-merkledag/test"
 
-	util "gx/ipfs/QmPdKqUcHGFdeSpvjVoaTRPPstGif9GBZb5Q56RVw9o69A/go-ipfs-util"
 	ipld "gx/ipfs/QmZtNq8dArGfnpCZfx2pUNY7UcjGhVp5qqwQ4hH6mpTMRQ/go-ipld-format"
 )
 
 func randNode() *merkledag.ProtoNode {
 	node := new(merkledag.ProtoNode)
 	node.SetData(make([]byte, 32))
-	util.NewTimeSeededRand().Read(node.Data())
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r.Read(node.Data())
 	return node
 }
 
