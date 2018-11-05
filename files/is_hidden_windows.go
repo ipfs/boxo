49d7a0e0dd0320d2c8d1ba4d4a310d7515ce5585
Łukasz Magiera <magik6k@gmail.com>
2018-11-05 17:05:53 +0100

    fix IsHidden on windows
    This commit was moved from ipfs/go-ipfs-files@b0b422eb2d7f4defd547e4cb93fbc0fe2023c0b7

--- files/is_hidden_windows.go
+++ files/is_hidden_windows.go
@@ -11,7 +11,7 @@ import (
 
 func IsHidden(name string, f File) bool {
 
-	fName := filepath.Base(f.FileName())
+	fName := filepath.Base(name)
 
 	if strings.HasPrefix(fName, ".") && len(fName) > 1 {
 		return true
