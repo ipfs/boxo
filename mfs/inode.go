a632c4c8b4bc4708bb45cdab8a6d5b18f5423f1b
Steven Allen <steven@stebalien.com>
2018-12-11 12:43:14 -0800

    go fmt
    This commit was moved from ipfs/go-mfs@06fd27e475c85a704f5c80fffaca4d5d7baea6c5

--- mfs/inode.go
+++ mfs/inode.go
@@ -10,10 +10,10 @@ import (
 type inode struct {
 	// name of this `inode` in the MFS path (the same value
 	// is also stored as the name of the DAG link).
-	name       string
+	name string
 
 	// parent directory of this `inode` (which may be the `Root`).
-	parent     childCloser
+	parent childCloser
 
 	// dagService used to store modifications made to the contents
 	// of the file or directory the `inode` belongs to.
