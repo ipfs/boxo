e5205c486694dad9f040999867c99472a72fdc01
Łukasz Magiera <magik6k@gmail.com>
2019-01-24 20:36:18 +0100

    generate archives from files instead of dag nodes
    This commit was moved from ipfs/go-unixfs@de8cb1497905d51fd13d6165ba0069a596ccbe70

--- unixfs/archive/archive.go
+++ unixfs/archive/archive.go
@@ -2,14 +2,11 @@
 package archive
 
 import (
-	"archive/tar"
 	"bufio"
 	"compress/gzip"
 	"errors"
-	"fmt"
 	"io"
 	"path"
-	"time"
 
 	files "github.com/ipfs/go-ipfs-files"
 )
@@ -81,7 +78,7 @@ func FileArchive(f files.Node, name string, archive bool, compression int) (io.R
 		// the case for 1. archive, and 2. not archived and not compressed, in which tar is used anyway as a transport format
 
 		// construct the tar writer
-		w, err := NewWriter(maybeGzw)
+		w, err := files.NewTarWriter(maybeGzw)
 		if checkErrAndClosePipe(err) {
 			return nil, err
 		}
@@ -104,97 +101,5 @@ func newMaybeGzWriter(w io.Writer, compression int) (io.WriteCloser, error) {
 		return gzip.NewWriterLevel(w, compression)
 	}
 	return &identityWriteCloser{w}, nil
-}
-
-type Writer struct {
-
-	TarW *tar.Writer
-}
-
-// NewWriter wraps given io.Writer.
-func NewWriter(w io.Writer) (*Writer, error) {
-	return &Writer{
-		TarW: tar.NewWriter(w),
-	}, nil
-}
-
-func (w *Writer) writeDir(f files.Directory, fpath string) error {
-	if err := writeDirHeader(w.TarW, fpath); err != nil {
-		return err
-	}
-
-	it := f.Entries()
-	for it.Next() {
-		if err := w.WriteFile(it.Node(), path.Join(fpath, it.Name())); err != nil {
-			return err
-		}
-	}
-	return it.Err()
-}
-
-func (w *Writer) writeFile(f files.File, fpath string) error {
-	size, err := f.Size()
-	if err != nil {
-		return err
-	}
-
-	if err := writeFileHeader(w.TarW, fpath, uint64(size)); err != nil {
-		return err
-	}
-
-	if _, err := io.Copy(w.TarW, f); err != nil {
-		return err
-	}
-	w.TarW.Flush()
-	return nil
-}
-
-// WriteNode adds a node to the archive.
-func (w *Writer) WriteFile(nd files.Node, fpath string) error {
-	switch nd := nd.(type) {
-	case *files.Symlink:
-		return writeSymlinkHeader(w.TarW, nd.Target, fpath)
-	case files.File:
-		return w.writeFile(nd, fpath)
-	case files.Directory:
-		return w.writeDir(nd, fpath)
-	default:
-		return fmt.Errorf("file type %T is not supported", nd)
-	}
-}
-
-// Close closes the tar writer.
-func (w *Writer) Close() error {
-	return w.TarW.Close()
-}
-
-func writeDirHeader(w *tar.Writer, fpath string) error {
-	return w.WriteHeader(&tar.Header{
-		Name:     fpath,
-		Typeflag: tar.TypeDir,
-		Mode:     0777,
-		ModTime:  time.Now(),
-		// TODO: set mode, dates, etc. when added to unixFS
-	})
-}
-
-func writeFileHeader(w *tar.Writer, fpath string, size uint64) error {
-	return w.WriteHeader(&tar.Header{
-		Name:     fpath,
-		Size:     int64(size),
-		Typeflag: tar.TypeReg,
-		Mode:     0644,
-		ModTime:  time.Now(),
-		// TODO: set mode, dates, etc. when added to unixFS
-	})
-}
-
-func writeSymlinkHeader(w *tar.Writer, target, fpath string) error {
-	return w.WriteHeader(&tar.Header{
-		Name:     fpath,
-		Linkname: target,
-		Mode:     0777,
-		Typeflag: tar.TypeSymlink,
-	})
 }
 
