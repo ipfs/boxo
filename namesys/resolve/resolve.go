f30c65a3877c3a9f7be87adbdf54a92554f85c6d
Łukasz Magiera <magik6k@gmail.com>
2019-04-03 03:51:45 +0200

    Move pathresolve
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/go-namesys@2479384173ecb39a5b133b1a6178e6f82ee324b8

---
+++ namesys/resolve/resolve.go
@@ -0,0 +1,78 @@
+package resolve
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/ipfs/go-ipld-format"
+	log2 "github.com/ipfs/go-log"
+	logging "github.com/ipfs/go-log"
+	"github.com/ipfs/go-path"
+	"github.com/ipfs/go-path/resolver"
+
+	"github.com/ipfs/go-ipfs/namesys"
+)
+
+var log = logging.Logger("nsresolv")
+
+// ErrNoNamesys is an explicit error for when an IPFS node doesn't
+// (yet) have a name system
+var ErrNoNamesys = errors.New(
+	"core/resolve: no Namesys on IpfsNode - can't resolve ipns entry")
+
+// ResolveIPNS resolves /ipns paths
+func ResolveIPNS(ctx context.Context, nsys namesys.NameSystem, p path.Path) (path.Path, error) {
+	if strings.HasPrefix(p.String(), "/ipns/") {
+		evt := log.EventBegin(ctx, "resolveIpnsPath")
+		defer evt.Done()
+		// resolve ipns paths
+
+		// TODO(cryptix): we should be able to query the local cache for the path
+		if nsys == nil {
+			evt.Append(log2.LoggableMap{"error": ErrNoNamesys.Error()})
+			return "", ErrNoNamesys
+		}
+
+		seg := p.Segments()
+
+		if len(seg) < 2 || seg[1] == "" { // just "/<protocol/>" without further segments
+			evt.Append(log2.LoggableMap{"error": path.ErrNoComponents.Error()})
+			return "", path.ErrNoComponents
+		}
+
+		extensions := seg[2:]
+		resolvable, err := path.FromSegments("/", seg[0], seg[1])
+		if err != nil {
+			evt.Append(log2.LoggableMap{"error": err.Error()})
+			return "", err
+		}
+
+		respath, err := nsys.Resolve(ctx, resolvable.String())
+		if err != nil {
+			evt.Append(log2.LoggableMap{"error": err.Error()})
+			return "", err
+		}
+
+		segments := append(respath.Segments(), extensions...)
+		p, err = path.FromSegments("/", segments...)
+		if err != nil {
+			evt.Append(log2.LoggableMap{"error": err.Error()})
+			return "", err
+		}
+	}
+	return p, nil
+}
+
+// Resolve resolves the given path by parsing out protocol-specific
+// entries (e.g. /ipns/<node-key>) and then going through the /ipfs/
+// entries and returning the final node.
+func Resolve(ctx context.Context, nsys namesys.NameSystem, r *resolver.Resolver, p path.Path) (format.Node, error) {
+	p, err := ResolveIPNS(ctx, nsys, p)
+	if err != nil {
+		return nil, err
+	}
+
+	// ok, we have an IPFS path now (or what we'll treat as one)
+	return r.ResolvePath(ctx, p)
+}
