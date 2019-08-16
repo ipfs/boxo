fc2a1a210b1758cc89ffa71fbe0a234d7123b824
Steven Allen <steven@stebalien.com>
2019-08-15 17:42:13 -0700

    serialfile: fix handling of hidden paths on windows
    fixes #11
    
    
    This commit was moved from ipfs/go-ipfs-files@a7bc21a37b777217e30b332761593c7dfbab0216

--- files/is_hidden_windows.go
+++ files/is_hidden_windows.go
@@ -3,33 +3,26 @@
 package files
 
 import (
-	"path/filepath"
-	"strings"
+	"os"
 
 	windows "golang.org/x/sys/windows"
 )
 
-func IsHidden(name string, f Node) bool {
+func isHidden(fi os.FileInfo) bool {
+	fName := fi.Name()
+	switch fName {
+	case "", ".", "..":
+		return false
+	}
 
-	fName := filepath.Base(name)
-
-	if strings.HasPrefix(fName, ".") && len(fName) > 1 {
+	if fName[0] == '.' {
 		return true
 	}
 
-	fi, ok := f.(FileInfo)
+	wi, ok := fi.Sys().(*windows.Win32FileAttributeData)
 	if !ok {
 		return false
 	}
 
-	p, e := windows.UTF16PtrFromString(fi.AbsPath())
-	if e != nil {
-		return false
-	}
-
-	attrs, e := windows.GetFileAttributes(p)
-	if e != nil {
-		return false
-	}
-	return attrs&windows.FILE_ATTRIBUTE_HIDDEN != 0
+	return wi.FileAttributes&windows.FILE_ATTRIBUTE_HIDDEN != 0
 }
