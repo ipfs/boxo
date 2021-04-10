d921b23d26bf41872c7b7682592989d6df4f74d6
hannahhoward <hannah@hannahhoward.net>
2021-04-09 19:54:15 -0700

    style(fetcher): add param names
    update parameter names on interface to be more clear
    
    
    This commit was moved from ipfs/go-fetcher@5325cff258c02c11f7ce7107070a0bc89bdbb35a

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -9,21 +9,26 @@ import (
 // Fetcher is an interface for reading from a dag. Reads may be local or remote, and may employ data exchange
 // protocols like graphsync and bitswap
 type Fetcher interface {
-	// NodeMatching traverses a node graph starting with the provided node using the given selector and possibly crossing
-	// block boundaries. Each matched node is passed as FetchResult to the callback. Errors returned from callback will
-	// halt the traversal. The sequence of events is: NodeMatching begins, the callback is called zero or more times
-	// with a FetchResult, then NodeMatching returns.
-	NodeMatching(context.Context, ipld.Node, ipld.Node, FetchCallback) error
+	// NodeMatching traverses a node graph starting with the provided root node using the given selector node and
+	// possibly crossing block boundaries. Each matched node is passed as FetchResult to the callback. Errors returned
+	// from callback will halt the traversal. The sequence of events is: NodeMatching begins, the callback is called zero
+	// or more times with a FetchResult, then NodeMatching returns.
+	NodeMatching(ctx context.Context, root ipld.Node, selector ipld.Node, cb FetchCallback) error
 
 	// BlockOfType fetches a node graph of the provided type corresponding to single block by link.
-	BlockOfType(context.Context, ipld.Link, ipld.NodePrototype) (ipld.Node, error)
+	BlockOfType(ctx context.Context, link ipld.Link, nodePrototype ipld.NodePrototype) (ipld.Node, error)
 
-	// BlockMatchingOfType traverses a node graph starting with the given link using the given selector and possibly
-	// crossing block boundaries. The nodes will be typed using the provided prototype. Each matched node is passed as
-	// a FetchResult to the callback. Errors returned from callback will halt the traversal.
+	// BlockMatchingOfType traverses a node graph starting with the given root link using the given selector node and
+	// possibly crossing block boundaries. The nodes will be typed using the provided prototype. Each matched node is
+	// passed as a FetchResult to the callback. Errors returned from callback will halt the traversal.
 	// The sequence of events is: BlockMatchingOfType begins, the callback is called zero or more times with a
 	// FetchResult, then BlockMatchingOfType returns.
-	BlockMatchingOfType(context.Context, ipld.Link, ipld.Node, ipld.NodePrototype, FetchCallback) error
+	BlockMatchingOfType(
+		ctx context.Context,
+		root ipld.Link,
+		selector ipld.Node,
+		nodePrototype ipld.NodePrototype,
+		cb FetchCallback) error
 
 	// Uses the given link to pick a prototype to build the linked node.
 	PrototypeFromLink(link ipld.Link) (ipld.NodePrototype, error)
