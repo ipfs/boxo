2d24be205ddb4154ec70781035ee6eb6cfce8d19
Masih H. Derkani <m@derkani.org>
2021-06-09 13:54:08 +0100

    Implement CAR v2 header construction and marshalling
    Define a constructor with sensible defaults, and the ability to
    customize the defaults conveniently.
    
    Implement `io.WriterTo` interface for both header and characteristics to
    provide a consistent standard API for writing data into a given
    `io.Writer`.
    
    
    This commit was moved from ipld/go-car@e571973176cfe57e86a6c5f92e84477c62376757

--- ipld/car/v2/car.go
+++ ipld/car/v2/car.go
@@ -1,10 +1,15 @@
 package car
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 const (
 	// HeaderBytesSize is the fixed size of CAR v2 header in number of bytes.
-	HeaderBytesSize = 40
+	HeaderBytesSize uint64 = 40
 	// CharacteristicsBytesSize is the fixed size of Characteristics bitfield within CAR v2 header in number of bytes.
-	CharacteristicsBytesSize = 16
+	CharacteristicsBytesSize uint64 = 16
 )
 
 var (
@@ -17,7 +22,7 @@ var (
 		0x02, // uint(2)
 	}
 	// The size of the CAR v2 prefix in 11 bytes, (i.e. 11).
-	PrefixBytesSize = len(PrefixBytes)
+	PrefixBytesSize = uint64(len(PrefixBytes))
 	// Reserved 128 bits space to capture future characteristics of CAR v2 such as order, duplication, etc.
 	EmptyCharacteristics = new(Characteristics)
 )
@@ -25,8 +30,9 @@ var (
 type (
 	// Header represents the CAR v2 header/pragma.
 	Header struct {
+		io.WriterTo
 		// 128-bit characteristics of this CAR v2 file, such as order, deduplication, etc. Reserved for future use.
-		Characteristics Characteristics
+		Characteristics *Characteristics
 		// The offset from the beginning of the file at which the dump of CAR v1 starts.
 		CarV1Offset uint64
 		// The size of CAR v1 encapsulated in this CAR v2 as bytes.
@@ -36,17 +42,100 @@ type (
 	}
 	// Characteristics is a bitfield placeholder for capturing the characteristics of a CAR v2 such as order and determinism.
 	Characteristics struct {
+		io.WriterTo
 		Hi uint64
 		Lo uint64
 	}
 )
 
-// Size gets the size of Header in number of bytes.
-func (h *Header) Size() int {
-	return HeaderBytesSize
+// WriteTo writes this characteristics to the given writer.
+func (c *Characteristics) WriteTo(w io.Writer) (n int64, err error) {
+	wn, err := writeUint64To(w, c.Hi)
+	if err != nil {
+		return
+	}
+	n += wn
+	wn, err = writeUint64To(w, c.Lo)
+	if err != nil {
+		return
+	}
+	n += wn
+	return
 }
 
 // Size gets the size of Characteristics in number of bytes.
-func (c *Characteristics) Size() int {
+func (c *Characteristics) Size() uint64 {
 	return CharacteristicsBytesSize
+}
+
+// NewHeader instantiates a new CAR v2 header, given the byte length of a CAR v1.
+func NewHeader(carV1Size uint64) *Header {
+	header := &Header{
+		Characteristics: EmptyCharacteristics,
+		CarV1Size:       carV1Size,
+	}
+	header.CarV1Offset = PrefixBytesSize + HeaderBytesSize
+	header.IndexOffset = header.CarV1Offset + carV1Size
+	return header
+}
+
+// Size gets the size of Header in number of bytes.
+func (h *Header) Size() uint64 {
+	return HeaderBytesSize
+}
+
+// WithIndexPadding sets the index offset from the beginning of the file for this header and returns the
+// header for convenient chained calls.
+// The index offset is calculated as the sum of PrefixBytesLen, HeaderBytesLen,
+// Header#CarV1Len, and the given padding.
+func (h *Header) WithIndexPadding(padding uint64) *Header {
+	h.IndexOffset = h.IndexOffset + padding
+	return h
+}
+
+// WithCarV1Padding sets the CAR v1 dump offset from the beginning of the file for this header and returns the
+// header for convenient chained calls.
+// The CAR v1 offset is calculated as the sum of PrefixBytesLen, HeaderBytesLen and the given padding.
+// The call to this function also shifts the Header#IndexOffset forward by the given padding.
+func (h *Header) WithCarV1Padding(padding uint64) *Header {
+	h.CarV1Offset = h.CarV1Offset + padding
+	h.IndexOffset = h.IndexOffset + padding
+	return h
+}
+
+// WriteTo serializes this header as bytes and writes them using the given io.Writer.
+func (h *Header) WriteTo(w io.Writer) (n int64, err error) {
+	chars := h.Characteristics
+	if chars == nil {
+		chars = EmptyCharacteristics
+	}
+	wn, err := chars.WriteTo(w)
+	if err != nil {
+		return
+	}
+	n += wn
+	wn, err = writeUint64To(w, h.CarV1Offset)
+	if err != nil {
+		return
+	}
+	n += wn
+	wn, err = writeUint64To(w, h.CarV1Size)
+	if err != nil {
+		return
+	}
+	n += wn
+	wn, err = writeUint64To(w, h.IndexOffset)
+	if err != nil {
+		return
+	}
+	n += wn
+	return
+}
+
+func writeUint64To(w io.Writer, v uint64) (n int64, err error) {
+	err = binary.Write(w, binary.LittleEndian, v)
+	if err == nil {
+		n = 8
+	}
+	return
 }
--- ipld/car/v2/car_test.go
+++ ipld/car/v2/car_test.go
@@ -1,6 +1,7 @@
 package car_test
 
 import (
+	"bytes"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	car_v1 "github.com/ipld/go-car"
 	car_v2 "github.com/ipld/go-car/v2"
@@ -56,7 +57,7 @@ func TestEmptyCharacteristics(t *testing.T) {
 	}{
 		{
 			"is of size 16 bytes",
-			16,
+			car_v2.CharacteristicsBytesSize,
 			car_v2.EmptyCharacteristics.Size(),
 		},
 		{
@@ -74,5 +75,123 @@ func TestEmptyCharacteristics(t *testing.T) {
 }
 
 func TestHeader_SizeIs40Bytes(t *testing.T) {
-	assert.Equal(t, 40, new(car_v2.Header).Size())
+	assert.Equal(t, uint64(40), new(car_v2.Header).Size())
+}
+
+func TestHeader_Marshal(t *testing.T) {
+	tests := []struct {
+		name        string
+		target      car_v2.Header
+		wantMarshal []byte
+		wantErr     bool
+	}{
+		{
+			"header with nil characteristics is marshalled as empty characteristics",
+			car_v2.Header{
+				Characteristics: nil,
+			},
+			[]byte{
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+			},
+			false,
+		},
+		{
+			"header with empty characteristics is marshalled as expected",
+			car_v2.Header{
+				Characteristics: car_v2.EmptyCharacteristics,
+			},
+			[]byte{
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+			},
+			false,
+		},
+		{
+			"non-empty header is marshalled as expected",
+			car_v2.Header{
+				Characteristics: &car_v2.Characteristics{
+					Hi: 1001, Lo: 1002,
+				},
+				CarV1Offset: 99,
+				CarV1Size:   100,
+				IndexOffset: 101,
+			},
+			[]byte{
+				0xe9, 0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0xea, 0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x63, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x64, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x65, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			written, err := tt.target.WriteTo(buf)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Marshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			gotMarshal := buf.Bytes()
+			assert.Equal(t, tt.wantMarshal, gotMarshal, "Header.WriteTo() gotMarshal = %v, wantMarshal %v", gotMarshal, tt.wantMarshal)
+			assert.EqualValues(t, car_v2.HeaderBytesSize, uint64(len(gotMarshal)), "WriteTo() CAR v2 header length must always be %v bytes long", car_v2.HeaderBytesSize)
+			assert.EqualValues(t, car_v2.HeaderBytesSize, uint64(written), "WriteTo() CAR v2 header byte count must always be %v bytes long", car_v2.HeaderBytesSize)
+		})
+	}
+}
+
+func TestHeader_WithPadding(t *testing.T) {
+	tests := []struct {
+		name            string
+		subject         *car_v2.Header
+		wantCarV1Offset uint64
+		wantIndexOffset uint64
+	}{
+		{
+			"when no padding, offsets are sum of sizes",
+			car_v2.NewHeader(123),
+			car_v2.PrefixBytesSize + car_v2.HeaderBytesSize,
+			car_v2.PrefixBytesSize + car_v2.HeaderBytesSize + 123,
+		},
+		{
+			"when only padding car v1, both offsets shift",
+			car_v2.NewHeader(123).WithCarV1Padding(3),
+			car_v2.PrefixBytesSize + car_v2.HeaderBytesSize + 3,
+			car_v2.PrefixBytesSize + car_v2.HeaderBytesSize + 3 + 123,
+		},
+		{
+			"when padding both car v1 and index, both offsets shift with additional index shift",
+			car_v2.NewHeader(123).WithCarV1Padding(3).WithIndexPadding(7),
+			car_v2.PrefixBytesSize + car_v2.HeaderBytesSize + 3,
+			car_v2.PrefixBytesSize + car_v2.HeaderBytesSize + 3 + 123 + 7,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.EqualValues(t, tt.wantCarV1Offset, tt.subject.CarV1Offset)
+			assert.EqualValues(t, tt.wantIndexOffset, tt.subject.IndexOffset)
+		})
+	}
+}
+
+func TestNewHeaderHasExpectedValues(t *testing.T) {
+	wantCarV1Len := uint64(1413)
+	want := &car_v2.Header{
+		Characteristics: car_v2.EmptyCharacteristics,
+		CarV1Offset:     car_v2.PrefixBytesSize + car_v2.HeaderBytesSize,
+		CarV1Size:       wantCarV1Len,
+		IndexOffset:     car_v2.PrefixBytesSize + car_v2.HeaderBytesSize + wantCarV1Len,
+	}
+	got := car_v2.NewHeader(wantCarV1Len)
+	assert.Equal(t, want, got, "NewHeader got = %v, want = %v", got, want)
 }
