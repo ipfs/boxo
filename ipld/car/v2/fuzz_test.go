a1f8ca251b06853c2bc64a8cb601fe6b951e77bc
Jorropo <jorropo.pgm@gmail.com>
2022-06-17 01:23:10 +0200

    test: v2 add fuzzing to Reader
    This commit was moved from ipld/go-car@b80929dd7ee763597b6d1a2a4b01bf5af176973b

--- ipld/car/v2/fuzz_test.go
+++ ipld/car/v2/fuzz_test.go
@@ -12,12 +12,13 @@ import (
 	"testing"
 
 	car "github.com/ipld/go-car/v2"
+	"github.com/ipld/go-car/v2/index"
 )
 
 // v1FixtureStr is a clean carv1 single-block, single-root CAR
 const v1FixtureStr = "3aa265726f6f747381d82a58250001711220151fe9e73c6267a7060c6f6c4cca943c236f4b196723489608edb42a8b8fa80b6776657273696f6e012c01711220151fe9e73c6267a7060c6f6c4cca943c236f4b196723489608edb42a8b8fa80ba165646f646779f5"
 
-func FuzzBlockReader(f *testing.F) {
+func seedWithCarFiles(f *testing.F) {
 	fixture, err := hex.DecodeString(v1FixtureStr)
 	if err != nil {
 		f.Fatal(err)
@@ -41,6 +42,10 @@ func FuzzBlockReader(f *testing.F) {
 			f.Add(data)
 		}()
 	}
+}
+
+func FuzzBlockReader(f *testing.F) {
+	seedWithCarFiles(f)
 
 	f.Fuzz(func(t *testing.T, data []byte) {
 		r, err := car.NewBlockReader(bytes.NewReader(data))
@@ -54,5 +59,23 @@ func FuzzBlockReader(f *testing.F) {
 				return
 			}
 		}
+	})
+}
+
+func FuzzReader(f *testing.F) {
+	seedWithCarFiles(f)
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		subject, err := car.NewReader(bytes.NewReader(data))
+		if err != nil {
+			return
+		}
+
+		subject.Roots()
+		ir := subject.IndexReader()
+		if ir != nil {
+			index.ReadFrom(ir)
+		}
+		car.GenerateIndex(subject.DataReader())
 	})
 }
