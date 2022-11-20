a8baf452edb9062caa14c4512346ef8db0ac1296
Marten Seemann <martenseemann@gmail.com>
2022-11-21 10:11:01 +1300

    chore: update go-libp2p
    This commit was moved from ipfs/go-ipfs-keystore@dea784f37c219c0478105d3ac78a5e6a75935327

--- keystore/keystore_test.go
+++ keystore/keystore_test.go
@@ -8,7 +8,7 @@ import (
 	"sort"
 	"testing"
 
-	ci "github.com/libp2p/go-libp2p-core/crypto"
+	ci "github.com/libp2p/go-libp2p/core/crypto"
 )
 
 type rr struct{}
@@ -160,7 +160,7 @@ func TestInvalidKeyFiles(t *testing.T) {
 
 	key := privKeyOrFatal(t)
 
-	bytes, err := key.Bytes()
+	bytes, err := key.Raw()
 	if err != nil {
 		t.Fatal(err)
 	}
