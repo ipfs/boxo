75cae5955740ec17a2b4c604596c1e389584055c
Antonio Navarro Perez <antnavper@gmail.com>
2022-11-22 13:18:34 +0100

    Fix go vet
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>
    
    
    This commit was moved from ipfs/go-delegated-routing@682995ceaa5f55cb3f95ce874031d9a720423dfc

---
+++ routing/http/types_test.go
@@ -0,0 +1,17 @@
+package delegatedrouting
+
+import (
+	"testing"
+)
+
+// TODO
+func TestThing(t *testing.T) {
+	// r := BitswapReadProviderRecord{
+	// 	TransferProtocol: TransferProtocol{Protocol: "proto"},
+	// }
+	// b, err := json.Marshal(r)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("%s\n", string(b))
+}
