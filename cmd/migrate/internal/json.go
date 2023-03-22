ac0938bc979283135d10e062c36a68f8e9314fc9
Jorropo <jorropo.pgm@gmail.com>
2023-03-22 05:22:44 +0100

    cmd/migrate: add support for XTestGoFiles
    This is for files that use `package packagename_test` to declare a 
    segregated test package in the same folder.

--- cmd/migrate/internal/json.go
+++ cmd/migrate/internal/json.go
@@ -7,12 +7,13 @@ type pkgJSON struct {
 	GoFiles        []string
 	IgnoredGoFiles []string
 	TestGoFiles    []string
+	XTestGoFiles   []string
 	CgoFiles       []string
 }
 
 func (p *pkgJSON) allSourceFiles() []string {
 	var files []string
-	lists := [][]string{p.GoFiles, p.IgnoredGoFiles, p.TestGoFiles, p.CgoFiles}
+	lists := [...][]string{p.GoFiles, p.IgnoredGoFiles, p.TestGoFiles, p.CgoFiles, p.XTestGoFiles}
 	for _, l := range lists {
 		for _, f := range l {
 			files = append(files, filepath.Join(p.Dir, f))
