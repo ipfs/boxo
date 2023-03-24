99992da27dcb25aa43064f66b6fa14046ac74b5e
Jorropo <jorropo.pgm@gmail.com>
2023-03-24 17:01:54 +0100

    cmd/migrate: unmigrate github.com/ipfs/go-block-format

--- cmd/migrate/internal/config.go
+++ cmd/migrate/internal/config.go
@@ -16,7 +16,6 @@ var DefaultConfig = Config{
 		"github.com/ipfs/go-bitswap":                     "github.com/ipfs/boxo/bitswap",
 		"github.com/ipfs/go-ipfs-files":                  "github.com/ipfs/boxo/files",
 		"github.com/ipfs/tar-utils":                      "github.com/ipfs/boxo/tar",
-		"github.com/ipfs/go-block-format":                "github.com/ipfs/boxo/blocks",
 		"github.com/ipfs/interface-go-ipfs-core":         "github.com/ipfs/boxo/coreiface",
 		"github.com/ipfs/go-unixfs":                      "github.com/ipfs/boxo/ipld/unixfs",
 		"github.com/ipfs/go-pinning-service-http-client": "github.com/ipfs/boxo/pinning/remote/client",
@@ -41,14 +40,12 @@ var DefaultConfig = Config{
 		"github.com/ipfs/go-ipfs-exchange-interface":     "github.com/ipfs/boxo/exchange",
 		"github.com/ipfs/go-merkledag":                   "github.com/ipfs/boxo/ipld/merkledag",
 		"github.com/ipld/go-car":                         "github.com/ipfs/boxo/ipld/car",
-		"github.com/ipfs/go-ipld-format":                 "github.com/ipfs/boxo/ipld/format",
 
 		// Pre Boxo rename
 		"github.com/ipfs/go-libipfs/gateway":               "github.com/ipfs/boxo/gateway",
 		"github.com/ipfs/go-libipfs/bitswap":               "github.com/ipfs/boxo/bitswap",
 		"github.com/ipfs/go-libipfs/files":                 "github.com/ipfs/boxo/files",
 		"github.com/ipfs/go-libipfs/tar":                   "github.com/ipfs/boxo/tar",
-		"github.com/ipfs/go-libipfs/blocks":                "github.com/ipfs/boxo/blocks",
 		"github.com/ipfs/go-libipfs/coreiface":             "github.com/ipfs/boxo/coreiface",
 		"github.com/ipfs/go-libipfs/unixfs":                "github.com/ipfs/boxo/ipld/unixfs",
 		"github.com/ipfs/go-libipfs/pinning/remote/client": "github.com/ipfs/boxo/pinning/remote/client",
@@ -71,6 +68,10 @@ var DefaultConfig = Config{
 		"github.com/ipfs/go-libipfs/exchange/offline":      "github.com/ipfs/boxo/exchange/offline",
 		"github.com/ipfs/go-libipfs/routing":               "github.com/ipfs/boxo/routing",
 		"github.com/ipfs/go-libipfs/exchange":              "github.com/ipfs/boxo/exchange",
+
+		// Unmigrated things
+		"github.com/ipfs/go-libipfs/blocks": "github.com/ipfs/go-block-format",
+		"github.com/ipfs/boxo/blocks":       "github.com/ipfs/go-block-format",
 	},
 	Modules: []string{
 		"github.com/ipfs/go-bitswap",
