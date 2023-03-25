ipfs/boxo

commit 4707f58b7e1c8be7ee33c5f1f44fae5326bb4193

cleanup: clean identity expression

entry.go
@@ -160,8 +160,7 @@ func (d *downloader) Read(b []byte) (int, error) {
 		pref := c.Prefix()
 		switch pref.MhType {
 		case mh.IDENTITY:
-			data = c.Hash()[1:]                   // skip the 0x00 prefix
-			data = data[len(data)-pref.MhLength:] // skip the multihash length
+			data = data[1+len(data)-pref.MhLength:] // skip the 0x00 prefix and the multihash length
 		default:
 			if err := verifcid.ValidateCid(c); err != nil {
 				return 0, fmt.Errorf("cid %s don't pass safe test: %w", cidStringTruncate(c), err)

commit 0169b3249e517bd14e78369efb5e40cf168bdd55

fix: identity expression
This fixes bug introduced in 4707f58b7e1c8be7ee33c5f1f44fae5326bb4193.

This is was completely wrong (does not load the digest and incorrectly truncate the data incorrectly.)

entry.go
@@ -160,7 +160,8 @@ func (d *downloader) Read(b []byte) (int, error) {
 		pref := c.Prefix()
 		switch pref.MhType {
 		case mh.IDENTITY:
-			data = data[1+len(data)-pref.MhLength:] // skip the 0x00 prefix and the multihash length
+			data = c.Hash()
+			data = data[len(data)-pref.MhLength:] // extract digest
 		default:
 			if err := verifcid.ValidateCid(c); err != nil {
 				return 0, fmt.Errorf("cid %s don't pass safe test: %w", cidStringTruncate(c), err)