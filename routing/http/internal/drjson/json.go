7240fcfa95e1772f752425d8edfa048affe62d12
Jorropo <jorropo.pgm@gmail.com>
2023-08-18 15:51:07 +0200

    routing/http: do not leak a bytes.Buffer in drjson

--- routing/http/internal/drjson/json.go
+++ routing/http/internal/drjson/json.go
@@ -5,22 +5,22 @@ import (
 	"encoding/json"
 )
 
-func marshalJSON(val any) (*bytes.Buffer, error) {
+func marshalJSON(val any) ([]byte, error) {
 	buf := &bytes.Buffer{}
 	enc := json.NewEncoder(buf)
 	enc.SetEscapeHTML(false)
 	err := enc.Encode(val)
-	return buf, err
+	return buf.Bytes(), err
 }
 
 // MarshalJSONBytes is needed to avoid changes
 // on the original bytes due to HTML escapes.
 func MarshalJSONBytes(val any) ([]byte, error) {
-	buf, err := marshalJSON(val)
+	bytes, err := marshalJSON(val)
 	if err != nil {
 		return nil, err
 	}
 
 	// remove last \n added by Encode
-	return buf.Bytes()[:buf.Len()-1], nil
+	return bytes[:len(bytes)-1], nil
 }
