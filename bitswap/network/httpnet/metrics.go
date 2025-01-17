9eb5bcac7a589a449556ee6faaf74d0e84eaf05d
Hector Sanjuan <code@hector.link>
2025-01-17 11:32:40 +0100

    bitswap/httpnet: use existing method to set metrics context

--- bitswap/network/httpnet/metrics.go
+++ bitswap/network/httpnet/metrics.go
@@ -15,63 +15,63 @@ type ctxKeyT string
 var ctxKey ctxKeyT = ctxKeyT(imetrics.CtxScopeKey)
 
 func requestsInFlight(ctx context.Context) imetrics.Gauge {
-	return imetrics.NewCtx(ctx, "httpnet_requests_in_flight", "Current number of in-flight requests").Gauge()
+	return imetrics.NewCtx(ctx, "requests_in_flight", "Current number of in-flight requests").Gauge()
 }
 
 func requestsTotal(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_requests_total", "Total request count").Counter()
+	return imetrics.NewCtx(ctx, "requests_total", "Total request count").Counter()
 }
 
 func requestsFailure(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_requests_failure", "Failed (no response, dial error etc) requests count").Counter()
+	return imetrics.NewCtx(ctx, "requests_failure", "Failed (no response, dial error etc) requests count").Counter()
 }
 
 func requestsBodyFailure(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_requests_body_failure", "Failure count when reading response body").Counter()
+	return imetrics.NewCtx(ctx, "requests_body_failure", "Failure count when reading response body").Counter()
 }
 
 func statusNotFound(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_404", "Request count with NotFound status").Counter()
+	return imetrics.NewCtx(ctx, "status_404", "Request count with NotFound status").Counter()
 }
 
 func statusGone(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_410", "Request count with Gone status").Counter()
+	return imetrics.NewCtx(ctx, "status_410", "Request count with Gone status").Counter()
 }
 
 func statusForbidden(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_403", "Request count with Forbidden status").Counter()
+	return imetrics.NewCtx(ctx, "status_403", "Request count with Forbidden status").Counter()
 }
 
 func statusUnavailableForLegalReasons(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_451", "Request count with Unavailable For Legal Reasons status").Counter()
+	return imetrics.NewCtx(ctx, "status_451", "Request count with Unavailable For Legal Reasons status").Counter()
 }
 
 func statusOK(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_200", "Request count with OK status").Counter()
+	return imetrics.NewCtx(ctx, "status_200", "Request count with OK status").Counter()
 }
 
 func statusTooManyRequests(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_429", "Request count with Too Many Requests status").Counter()
+	return imetrics.NewCtx(ctx, "status_429", "Request count with Too Many Requests status").Counter()
 }
 
 func statusServiceUnavailable(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_503", "Request count with Service Unavailable status").Counter()
+	return imetrics.NewCtx(ctx, "status_503", "Request count with Service Unavailable status").Counter()
 }
 
 func statusInternalServerError(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_500", "Request count with Internal Server Error status").Counter()
+	return imetrics.NewCtx(ctx, "status_500", "Request count with Internal Server Error status").Counter()
 }
 
 func statusOthers(ctx context.Context) imetrics.Counter {
-	return imetrics.NewCtx(ctx, "httpnet_status_others", "Request count with other status codes").Counter()
+	return imetrics.NewCtx(ctx, "status_others", "Request count with other status codes").Counter()
 }
 
 func requestTime(ctx context.Context) imetrics.Histogram {
-	return imetrics.NewCtx(ctx, "httpnet_request_duration_seconds", "Histogram of request durations").Histogram(durationHistogramBuckets)
+	return imetrics.NewCtx(ctx, "request_duration_seconds", "Histogram of request durations").Histogram(durationHistogramBuckets)
 }
 
 func responseSize(ctx context.Context) imetrics.Histogram {
-	return imetrics.NewCtx(ctx, "httpnet_response_bytes", "Histogram of http response sizes").Histogram(blockSizesHistogramBuckets)
+	return imetrics.NewCtx(ctx, "response_bytes", "Histogram of http response sizes").Histogram(blockSizesHistogramBuckets)
 }
 
 type metrics struct {
@@ -93,7 +93,7 @@ type metrics struct {
 }
 
 func newMetrics() *metrics {
-	ctx := context.WithValue(context.Background(), ctxKey, "exchange")
+	ctx := imetrics.CtxScope(context.Background(), "exchange_httpnet")
 
 	return &metrics{
 		RequestsInFlight:                 requestsInFlight(ctx),
