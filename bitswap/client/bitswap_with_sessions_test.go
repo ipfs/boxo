d508883950938b728dfb4f4e794c9bc29843f6d6
gammazero <11790789+gammazero@users.noreply.github.com>
2025-02-03 21:06:04 -1000

    increase timeout for flakey test

--- bitswap/client/bitswap_with_sessions_test.go
+++ bitswap/client/bitswap_with_sessions_test.go
@@ -318,7 +318,7 @@ func TestFetchNotConnected(t *testing.T) {
 }
 
 func TestFetchAfterDisconnect(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	vnet := getVirtualNetwork()
