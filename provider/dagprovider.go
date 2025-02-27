f802e47b5cef1f1039cd34eef71fa7c3b6cfe201
Hector Sanjuan <code@hector.link>
2025-02-27 15:53:18 +0100

    Provider: implement generic DagProvider
    The DAG provider creates a KeyChanFunc for all the CIDs found traversing the
    merkledag from given root.
    
    It can be used to provide specific DAGs (i.e. MFS) from their root.

--- provider/dagprovider.go
+++ provider/dagprovider.go
@@ -3,7 +3,6 @@ package provider
 import (
 	"context"
 	"fmt"
-	logger "log"
 
 	"github.com/ipfs/boxo/fetcher"
 	fetcherhelpers "github.com/ipfs/boxo/fetcher/helpers"
@@ -18,69 +17,32 @@ func NewDAGProvider(root cid.Cid, fetchConfig fetcher.Factory) KeyChanFunc {
 			return nil, fmt.Errorf("root CID cannot be empty")
 		}
 
-		outCh := make(chan cid.Cid, 64) // Add buffer to prevent blocking
 		set := cidutil.NewStreamingSet()
 
-		logger.Printf("Starting DAG traversal with root: %s", root)
-
-		// Use buffered channel for coordination
-		done := make(chan struct{}, 1)
-
 		go func() {
-			defer func() {
-				close(set.New)
-				done <- struct{}{} // Signal completion
-			}()
-
+			defer close(set.New)
 			session := fetchConfig.NewSession(ctx)
 			err := fetcherhelpers.BlockAll(ctx, session, cidlink.Link{Cid: root}, func(res fetcher.FetchResult) error {
+				clink, ok := res.LastBlockLink.(cidlink.Link)
+				if ok {
+					// if context is cancelled, nothing is written to new()
+					_ = set.Visitor(ctx)(clink.Cid)
+				}
+
 				select {
 				case <-ctx.Done():
+					// halts traversal
 					return ctx.Err()
 				default:
-					clink, ok := res.LastBlockLink.(cidlink.Link)
-					if !ok {
-						log.Warnf("unexpected link type: %T", res.LastBlockLink)
-						return nil
-					}
-					set.Visitor(ctx)(clink.Cid)
-					logger.Printf("Added CID to set: %s", clink.Cid)
-					return nil
 				}
+				return nil
 			})
 			if err != nil {
-				log.Errorf("dag traversal error: %s", err)
+				log.Errorf("dagprovider dag traversal error: %s", err)
 				return
 			}
-			logger.Printf("Completed BlockAll traversal")
 		}()
 
-		go func() {
-			defer close(outCh)
-
-			for {
-				select {
-				case c, ok := <-set.New:
-					if !ok {
-						select {
-						case <-done: // Wait for traversal to complete
-							return
-						case <-ctx.Done():
-							return
-						}
-					}
-					select {
-					case <-ctx.Done():
-						return
-					case outCh <- c:
-						logger.Printf("Sent CID to output: %s", c)
-					}
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
-
-		return outCh, nil
+		return set.New, nil
 	}
 }
--- provider/dagprovider_test.go
+++ provider/dagprovider_test.go
@@ -2,210 +2,87 @@ package provider
 
 import (
 	"context"
-	"fmt"
-	logger "log"
 	"testing"
 	"time"
 
 	"github.com/ipfs/boxo/blockservice"
-	"github.com/ipfs/boxo/fetcher"
-	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
-	"github.com/ipld/go-ipld-prime/node/basicnode"
-
+	"github.com/ipfs/boxo/blockstore"
+	"github.com/ipfs/boxo/exchange/offline"
+	bsfetcher "github.com/ipfs/boxo/fetcher/impl/blockservice"
 	"github.com/ipfs/boxo/ipld/merkledag"
 	mdutils "github.com/ipfs/boxo/ipld/merkledag/test"
 	"github.com/ipfs/go-cid"
-	ipldp "github.com/ipld/go-ipld-prime"
+	"github.com/ipfs/go-datastore"
+	dssync "github.com/ipfs/go-datastore/sync"
+	"github.com/ipfs/go-unixfsnode"
+	dagpb "github.com/ipld/go-codec-dagpb"
 	"github.com/stretchr/testify/require"
 )
 
-type mockFetcherFactory struct {
-	blockService blockservice.BlockService
-}
+func makeDAGProvider(t *testing.T, ctx context.Context) (KeyChanFunc, []cid.Cid) {
+	t.Helper()
 
-func (m *mockFetcherFactory) NewSession(ctx context.Context) fetcher.Fetcher {
-	return &mockFetcher{bs: m.blockService}
-}
+	ds := dssync.MutexWrap(datastore.NewMapDatastore())
+	bs := blockstore.NewBlockstore(ds)
+	bserv := blockservice.New(bs, offline.Exchange(bs))
+	ipldFetcher := bsfetcher.NewFetcherConfig(bserv)
+	ipldFetcher.PrototypeChooser = dagpb.AddSupportToChooser(bsfetcher.DefaultPrototypeChooser)
+	unixFSFetcher := ipldFetcher.WithReifier(unixfsnode.Reify)
+	dserv := merkledag.NewDAGService(bserv)
+	daggen := mdutils.NewDAGGenerator()
 
-type mockFetcher struct {
-	bs blockservice.BlockService
-}
+	root, allCids, err := daggen.MakeDagNode(dserv.Add, 5, 2)
+	require.NoError(t, err)
+	t.Logf("Generated %d CIDs", len(allCids))
 
-func (m *mockFetcher) BlockMatchingOfType(ctx context.Context, root ipldp.Link, selector ipldp.Node, _ ipldp.NodePrototype, cb fetcher.FetchCallback) error {
-	logger.Println("Starting BlockMatchingOfType")
-
-	visited := make(map[string]bool)
-	processed := 0
-
-	var traverse func(ipldp.Link) error
-	traverse = func(link ipldp.Link) error {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-		}
-
-		cidLnk, ok := link.(cidlink.Link)
-		if !ok {
-			return fmt.Errorf("expected CID link")
-		}
-
-		cidStr := cidLnk.Cid.String()
-		if visited[cidStr] {
-			logger.Printf("Already visited CID: %s", cidStr)
-			return nil
-		}
-
-		// Add select for context cancellation
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			blk, err := m.bs.GetBlock(ctx, cidLnk.Cid)
-			if err != nil {
-				logger.Printf("Error getting block %s: %s", cidStr, err)
-				return err
-			}
-
-			visited[cidStr] = true
-			processed++
-			logger.Printf("Processing CID %s (%d processed)", cidStr, processed)
-
-			// Add select for callback
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			default:
-				err = cb(fetcher.FetchResult{
-					Node:          basicnode.NewString(string(blk.RawData())),
-					LastBlockLink: link,
-				})
-				if err != nil {
-					logger.Printf("Callback error for CID %s: %s", cidStr, err)
-					return err
-				}
-			}
-
-			if protoNode, ok := blk.(*merkledag.ProtoNode); ok {
-				links := protoNode.Links()
-				logger.Printf("Found %d links in CID %s", len(links), cidStr)
-
-				for i, link := range links {
-					select {
-					case <-ctx.Done():
-						return ctx.Err()
-					default:
-						logger.Printf("Traversing child %d/%d of CID %s: %s",
-							i+1, len(links), cidStr, link.Cid)
-
-						if err := traverse(cidlink.Link{Cid: link.Cid}); err != nil {
-							return err
-						}
-					}
-				}
-			}
-		}
-
-		return nil
-	}
-
-	err := traverse(root)
-	if err != nil {
-		logger.Printf("Traversal error: %s", err)
-		return err
-	}
-
-	logger.Printf("Completed DAG traversal, processed %d CIDs", processed)
-	return nil
-}
-
-func (m *mockFetcher) BlockOfType(ctx context.Context, link ipldp.Link, nodePrototype ipldp.NodePrototype) (ipldp.Node, error) {
-	cidLnk, ok := link.(cidlink.Link)
-	if !ok {
-		return nil, fmt.Errorf("expected CID link")
-	}
-
-	blk, err := m.bs.GetBlock(ctx, cidLnk.Cid)
-	if err != nil {
-		return nil, err
-	}
-
-	builder := nodePrototype.NewBuilder()
-	err = builder.AssignBytes(blk.RawData())
-	if err != nil {
-		return nil, err
-	}
-	return builder.Build(), nil
-	// return nodePrototype.NewBuilder().AssignBytes(blk.RawData()).Build()
-}
-
-func (m *mockFetcher) NodeMatching(ctx context.Context, root ipldp.Node, selector ipldp.Node, cb fetcher.FetchCallback) error {
-	return cb(fetcher.FetchResult{
-		Node: root,
-	})
-}
-
-func (m *mockFetcher) PrototypeFromLink(link ipldp.Link) (ipldp.NodePrototype, error) {
-	return basicnode.Prototype.Any, nil
+	keyChanF := NewDAGProvider(root, unixFSFetcher)
+	return keyChanF, allCids
 }
 
 func TestNewDAGProvider(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// Create test DAG
-	dagGen := mdutils.NewDAGGenerator()
-	bsrv := mdutils.Bserv()
-
-	// Generate a DAG with fanout=2, depth=3
-	root, expectedCids, err := dagGen.MakeDagBlock(bsrv.AddBlock, 2, 3)
-	require.NoError(t, err)
-	t.Logf("Created DAG with root %s and %d expected CIDs", root, len(expectedCids))
-	for i, c := range expectedCids {
-		t.Logf("Expected CID %d: %s", i+1, c)
-	}
-
-	// Create mock fetcher using our root blockservice
-	fetcherFactory := &mockFetcherFactory{blockService: bsrv}
-
-	// Creating DAGProvider with our root and fetcher
-	provider := NewDAGProvider(root, fetcherFactory)
-	cidChan, err := provider(ctx)
+	ctx := context.Background()
+	keyChanF, allCids := makeDAGProvider(t, ctx)
+	cidMap := make(map[cid.Cid]struct{})
+	cidCh, err := keyChanF(ctx)
 	require.NoError(t, err)
 
-	// collect all the CIDs from the provider
-	var collectedCids []cid.Cid
-	for c := range cidChan {
-		t.Logf("Collected CID: %s", c)
-		collectedCids = append(collectedCids, c)
+	for c := range cidCh {
+		cidMap[c] = struct{}{}
 	}
 
-	t.Logf("Collected %d CIDs", len(collectedCids))
+	t.Logf("Collected %d CIDs", len(cidMap))
 
-	// Verify - Got all the CIDs
-	require.Equal(t, len(expectedCids), len(collectedCids), "number of collected CIDs should match the expected number")
-
-	// Creating maps for easier comparison
-	expectedMap := make(map[string]bool)
-	for _, c := range expectedCids {
-		expectedMap[c.String()] = true
+	for _, c := range allCids {
+		if _, ok := cidMap[c]; !ok {
+			t.Errorf("%s not traversed", c)
+		}
 	}
+	require.Equal(t, len(allCids), len(cidMap), "number of traversed CIDs does not match CIDs in DAG")
+}
 
-	collectedMap := make(map[string]bool)
-	for _, c := range collectedCids {
-		collectedMap[c.String()] = true
-	}
+func TestNewDAGProviderCtxCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	keyChanF, _ := makeDAGProvider(t, ctx)
+	cidMap := make(map[cid.Cid]struct{})
+	cidCh, err := keyChanF(ctx)
+	require.NoError(t, err)
 
-	// Verify each expected CID was collected
-	for _, expected := range expectedCids {
-		require.True(t, collectedMap[expected.String()],
-			"expected CID %s not found in collected CIDs", expected)
-	}
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for c := range cidCh {
+			cidMap[c] = struct{}{}
+			if len(cidMap) == 3 {
+				time.Sleep(time.Second)
+			}
+		}
 
-	// Verify no extra CIDs were collected
-	for _, collected := range collectedCids {
-		require.True(t, expectedMap[collected.String()],
-			"unexpected CID %s found in collected CIDs", collected)
-	}
+	}()
 
+	time.Sleep(500 * time.Millisecond)
+	cancel()
+	<-done
+
+	t.Logf("Collected %d CIDs", len(cidMap))
+	require.Equal(t, 3, len(cidMap), "number of traversed CIDs when cancelling the context should be 3")
 }
