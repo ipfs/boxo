1109c20f9241d059ae9260204d59149f731763a2
gammazero <11790789+gammazero@users.noreply.github.com>
2025-03-19 01:47:07 -1000

    Improve the perceived performance of data onboarding
    Adding a large directory of data to IPFS can take a long time. A significant amount of this time is spent writing to and reading from the persisted provider queue, depending on the underlying datastore. For slower datastores, buffering input to the persisted queue, in memory, allows data to be read in much more quickly. Overall performance is somewhat increased since writes are not blocked (up to a point) while waiting for the queue to persist data to the datastore.

--- provider/internal/queue/queue.go
+++ provider/internal/queue/queue.go
@@ -2,10 +2,13 @@ package queue
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"sync"
+	"time"
 
+	"github.com/gammazero/chanqueue"
 	cid "github.com/ipfs/go-cid"
 	datastore "github.com/ipfs/go-datastore"
 	namespace "github.com/ipfs/go-datastore/namespace"
@@ -15,96 +18,111 @@ import (
 
 var log = logging.Logger("provider.queue")
 
-// Queue provides a best-effort durability, FIFO interface to the datastore for storing cids
+const (
+	// Number of input CIDs to buffer without blocking.
+	inputBufferSize = 65536
+	// Time for Close to wait to finish writing CIDs to datastore.
+	shutdownTimeout = 5 * time.Second
+)
+
+// Queue provides a FIFO interface to the datastore for storing cids.
 //
-// Best-effort durability just means that cids in the process of being provided when a
-// crash or shutdown occurs may be in the queue when the node is brought back online
-// depending on whether the underlying datastore has synchronous or asynchronous writes.
+// Cids in the process of being provided when a crash or shutdown occurs may be
+// in the queue when the node is brought back online depending on whether they
+// were fully written to the underlying datastore.
+//
+// Input to the queue is buffered in memory, up to inputBufferSize, to increase
+// the speed of data onboarding. This input buffer behaves as a channel with a
+// very large capacity.
 type Queue struct {
 	// used to differentiate queues in datastore
 	// e.g. provider vs reprovider
-	ctx     context.Context
-	ds      datastore.Datastore // Must be threadsafe
-	dequeue chan cid.Cid
-	enqueue chan cid.Cid
-	close   context.CancelFunc
-	closed  sync.WaitGroup
-
-	counter uint64
+	ctx       context.Context
+	ds        datastore.Datastore // Must be threadsafe
+	dequeue   chan cid.Cid
+	enqueue   *chanqueue.ChanQueue[cid.Cid] // in-memory queue to buffer input
+	close     context.CancelFunc
+	closed    chan struct{}
+	closeOnce sync.Once
 }
 
 // NewQueue creates a queue for cids
 func NewQueue(ds datastore.Datastore) *Queue {
-	namespaced := namespace.Wrap(ds, datastore.NewKey("/queue"))
-	cancelCtx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 	q := &Queue{
-		ctx:     cancelCtx,
-		ds:      namespaced,
+		ds:      namespace.Wrap(ds, datastore.NewKey("/queue")),
 		dequeue: make(chan cid.Cid),
-		enqueue: make(chan cid.Cid),
+		//enqueue: make(chan cid.Cid, 1024),
+		enqueue: chanqueue.New(chanqueue.WithCapacity[cid.Cid](inputBufferSize)),
 		close:   cancel,
+		closed:  make(chan struct{}),
 	}
-	q.closed.Add(1)
-	go q.worker()
+	go q.worker(ctx)
 	return q
 }
 
 // Close stops the queue
 func (q *Queue) Close() error {
-	q.close()
-	q.closed.Wait()
-	// We don't close dequeue because the provider which consume this get caught in
-	// an infinite loop dequeing cid.Undef if we do that.
-	// The provider has it's own select on top of dequeue and will handle this by itself.
-	return nil
+	var err error
+	q.closeOnce.Do(func() {
+		// Close input queue and wait for worker to finish reading it.
+		q.enqueue.Close()
+		select {
+		case <-q.closed:
+		case <-time.After(shutdownTimeout):
+			q.close() // force immediate shutdown
+			<-q.closed
+			err = errors.New("provider queue: some cids not written to datastore")
+		}
+		close(q.dequeue) // no more output from this queue
+	})
+	return err
 }
 
 // Enqueue puts a cid in the queue
-func (q *Queue) Enqueue(cid cid.Cid) error {
-	select {
-	case q.enqueue <- cid:
-		return nil
-	case <-q.ctx.Done():
-		return errors.New("failed to enqueue CID: shutting down")
-	}
+func (q *Queue) Enqueue(cid cid.Cid) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("failed to enqueue CID: shutting down")
+		}
+	}()
+	q.enqueue.In() <- cid
+	return
 }
 
-// Dequeue returns a channel that if listened to will remove entries from the queue
+// Dequeue returns a channel that for reading entries from the queue,
 func (q *Queue) Dequeue() <-chan cid.Cid {
 	return q.dequeue
 }
 
 // worker run dequeues and enqueues when available.
-func (q *Queue) worker() {
+func (q *Queue) worker(ctx context.Context) {
+	defer close(q.closed)
+	defer q.enqueue.Shutdown()
+
 	var k datastore.Key = datastore.Key{}
 	var c cid.Cid = cid.Undef
-
-	defer q.closed.Done()
-	defer q.close()
+	var counter uint64
 
 	for {
 		if c == cid.Undef {
-			head, err := q.getQueueHead()
-
-			switch {
-			case err != nil:
+			head, err := q.getQueueHead(ctx)
+			if err != nil {
 				log.Errorf("error querying for head of queue: %s, stopping provider", err)
 				return
-			case head != nil:
+			}
+			if head != nil {
 				k = datastore.NewKey(head.Key)
 				c, err = cid.Parse(head.Value)
 				if err != nil {
 					log.Warnf("error parsing queue entry cid with key (%s), removing it from queue: %s", head.Key, err)
-					err = q.ds.Delete(q.ctx, k)
-					if err != nil {
+					if err = q.ds.Delete(ctx, k); err != nil {
 						log.Errorf("error deleting queue entry with key (%s), due to error (%s), stopping provider", head.Key, err)
 						return
 					}
 					continue
 				}
-			default:
-				c = cid.Undef
-			}
+			} // else queue is empty
 		}
 
 		// If c != cid.Undef set dequeue and attempt write, otherwise wait for enqueue
@@ -114,10 +132,16 @@ func (q *Queue) worker() {
 		}
 
 		select {
-		case toQueue := <-q.enqueue:
-			keyPath := fmt.Sprintf("%020d/%s", q.counter, c.String())
-			q.counter++
-			nextKey := datastore.NewKey(keyPath)
+		case toQueue, ok := <-q.enqueue.Out():
+			if !ok {
+				return
+			}
+			// Add unique suffix to key path to allow multiple entries with the
+			// same sequence.
+			toQueueBytes := toQueue.Bytes()
+			sfx := base64.RawURLEncoding.EncodeToString(toQueueBytes[len(toQueueBytes)-6:])
+			nextKey := datastore.NewKey(fmt.Sprintf("%020d/%s", counter, sfx))
+			counter++
 
 			if c == cid.Undef {
 				// fast path, skip rereading the datastore if we don't have anything in hand yet
@@ -125,26 +149,29 @@ func (q *Queue) worker() {
 				k = nextKey
 			}
 
-			if err := q.ds.Put(q.ctx, nextKey, toQueue.Bytes()); err != nil {
+			if err := q.ds.Put(ctx, nextKey, toQueueBytes); err != nil {
 				log.Errorf("Failed to enqueue cid: %s", err)
 				continue
 			}
 		case dequeue <- c:
-			err := q.ds.Delete(q.ctx, k)
+			err := q.ds.Delete(ctx, k)
 			if err != nil {
 				log.Errorf("Failed to delete queued cid %s with key %s: %s", c, k, err)
 				continue
 			}
 			c = cid.Undef
-		case <-q.ctx.Done():
+		case <-ctx.Done():
 			return
 		}
 	}
 }
 
-func (q *Queue) getQueueHead() (*query.Entry, error) {
-	qry := query.Query{Orders: []query.Order{query.OrderByKey{}}, Limit: 1}
-	results, err := q.ds.Query(q.ctx, qry)
+func (q *Queue) getQueueHead(ctx context.Context) (*query.Entry, error) {
+	qry := query.Query{
+		Orders: []query.Order{query.OrderByKey{}},
+		Limit:  1,
+	}
+	results, err := q.ds.Query(ctx, qry)
 	if err != nil {
 		return nil, err
 	}
