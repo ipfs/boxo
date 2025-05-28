1165dae30464856bad271b12ae89c81f4f9127cf
Hector Sanjuan <code@hector.link>
2025-05-28 12:11:43 +0200

    Allow potential compilation into wasm
    The modified file was the only thing in boxo erroring when attempting to build a wasm target

--- files/filewriter_js.go
+++ files/filewriter_js.go
@@ -1,11 +1,10 @@
-//go:build darwin || linux || netbsd || openbsd || freebsd || dragonfly
+//go:build js
 
 package files
 
 import (
 	"os"
 	"strings"
-	"syscall"
 )
 
 var invalidChars = `/` + "\x00"
@@ -15,5 +14,5 @@ func isValidFilename(filename string) bool {
 }
 
 func createNewFile(path string) (*os.File, error) {
-	return os.OpenFile(path, os.O_EXCL|os.O_CREATE|os.O_WRONLY|syscall.O_NOFOLLOW, 0o666)
+	return os.OpenFile(path, os.O_EXCL|os.O_CREATE|os.O_WRONLY, 0o666)
 }
